786a76ba19704e4d895c55732a9b04bbb58bdd2e
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-07-07 12:34:53 -0300

    ADICIONANDO RECURSOS

--- js/system.js
+++ js/system.js
@@ -11,11 +11,11 @@ function parcelamentos()
     <table>
     
         <tr>
-            <td class="head"> Nº </td> <td class="head">MÊS/ANO</td> <td class="head"> VENCIMENTO </td> <td class="head"> VALOR </td> <td class="head"> SITUAÇÃO </td> <td class="head"> REPASSE </td>
+            <td class="head"> Nº </td> <td class="head">MÊS/ANO</td> <td class="head"> VENCIMENTO </td> <td class="head"> VALOR </td> <td class="head"> SITUAÇÃO </td> <td class="head"> REPASSE </td> <td class="head"> ARQUIVO </td>
         </tr>
 
         <tr>
-            <td> 41/60 </td> <td> mai/23 </td> <td> 19/06/23 </td> <td> R$ 73 824,36 </td> <td class="loss"> ABERTO </td> <td> falta </td>
+            <td> 41/60 </td> <td> mai/23 </td> <td> 19/06/23 </td> <td> R$ 73 824,36 </td> <td class="loss"> ABERTO </td> <td class="loss"> falta </td> <td> ver </td> 
         </tr>
 
         <tr>
@@ -30,6 +30,14 @@ function parcelamentos()
             <td> 38/60 </td> <td> fev/23 </td> <td> 17/03/23 </td> <td> R$ 71 251,19 </td> <td class="win"> PAGO </td> <td> 10/03/23 </td>
         </tr>
 
+        <tr>
+            <td> 37/60 </td> <td> jan/23 </td> <td> 17/02/23 </td> <td> R$ 70 624,66 </td> <td class="win"> PAGO </td> <td> 10/02/23 </td>
+        </tr>
+
+        <tr>
+            <td> 36/60 </td> <td> dez/22 </td> <td> 17/01/23 </td> <td> R$ 70 326,67 </td> <td class="loss"> ABERTO </td> <td class="loss"> falta </td>
+        </tr>
+
     </table>
     
     
@@ -40,5 +48,10 @@ function parcelamentos()
 
 function reload()
 {
-    window.alert("ok")
+    location.reload()
+}
+
+function guias()
+{
+    window.alert("Em Breve")
 }
